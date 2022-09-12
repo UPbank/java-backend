cd7f215f4db5f4ab072643b6fe3b7b3782865dfd
joaosinfronio <joaobritosinfronio@gmail.com>
2022-09-12 12:31:43 +0100

    pull Login

--- upbank/src/main/java/pt/upbank/upbank/models/User.java
+++ upbank/src/main/java/pt/upbank/upbank/models/User.java
@@ -1,5 +1,13 @@
 package pt.upbank.upbank.models;
 
+import javax.persistence.Entity;
+
+import lombok.Data;
+
+@Data
+@Entity
 public class User {
+
+
     
 }
--- upbank/src/main/java/pt/upbank/upbank/repositories/AccountRepository.java
+++ upbank/src/main/java/pt/upbank/upbank/repositories/AccountRepository.java
@@ -1,12 +1,23 @@
 package pt.upbank.upbank.repositories;
 
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.transaction.annotation.Transactional;
+import java.util.List;
 
-public interface AccountRepository {
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import pt.upbank.upbank.models.Account;
+
+public interface AccountRepository extends JpaRepository<Account, Long>   {
     
-    //Find by user 
-    //Find by Id 
-    //Find by name
+
+    //If exists account with nib
+    boolean existsById(long account);
+    // boolean existsByNibId(long nib);
+
+    //accounts of a user 
+    Account  findByUserId(long userId);
+
+    Account findByPhoneNumber(long phoneNumber);
+
+
     
 }
