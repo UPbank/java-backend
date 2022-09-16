e0027525e84fc7bb71ec0e30b1b85fa6f5f486c9
Pedro Cavalheiro <pedro.cavalheiro@isat.pt>
2022-09-16 17:11:47 +0100

    Added a reference API spec

--- src/main/java/pt/ualg/upbank/HomeController.java
+++ src/main/java/pt/ualg/upbank/HomeController.java
@@ -1,9 +1,12 @@
 package pt.ualg.upbank;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
 import org.springframework.stereotype.Controller;
+import org.springframework.core.io.Resource;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
+import java.io.IOException;
 
 @Controller
 public class HomeController {
@@ -11,7 +14,14 @@ public class HomeController {
     @GetMapping("/")
     @ResponseBody
     public String index() {
-        return "This is the root of the Rest API. Please refer to the documentation for more information.";
+				Resource resource = new ClassPathResource("/static/api-reference.json");
+				try {
+						ObjectMapper mapper = new ObjectMapper();
+						return mapper.readValue(resource.getInputStream(), Object.class);
+				} catch (IOException e) {
+						e.printStackTrace();
+				}
+				return null;
     }
 
 }
