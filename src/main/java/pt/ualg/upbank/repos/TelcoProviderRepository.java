2d202085489cc5350d07d977210b0bd1462dca19
joaosinfronio <joaobritosinfronio@gmail.com>
2022-09-19 17:39:02 +0100

    Update TelcoProviderRepository.java

--- src/main/java/pt/ualg/upbank/repos/TelcoProviderRepository.java
+++ src/main/java/pt/ualg/upbank/repos/TelcoProviderRepository.java
@@ -1,8 +1,20 @@
 package pt.ualg.upbank.repos;
 
+import java.util.List;
+import java.util.Optional;
+
 import org.springframework.data.jpa.repository.JpaRepository;
+
 import pt.ualg.upbank.domain.TelcoProvider;
 
 
 public interface TelcoProviderRepository extends JpaRepository<TelcoProvider, Long> {
+
+    Optional <TelcoProvider> findById(Long id);
+    boolean existsById(Long Id);
+
+    Optional <List<TelcoProvider>> findByNameLike(String name);
+    
+    boolean existsByName(String name);
+
 }
