0f6f2beeb4270c0535a991c16e442003264dec52
Pedro Cavalheiro <pedro.cavalheiro@isat.pt>
2022-09-19 09:30:23 +0100

    Fixed an api reference error

--- src/main/java/pt/ualg/upbank/HomeController.java
+++ src/main/java/pt/ualg/upbank/HomeController.java
@@ -1,27 +1,25 @@
 package pt.ualg.upbank;
 
-import com.fasterxml.jackson.databind.ObjectMapper;
 import org.springframework.stereotype.Controller;
+import org.springframework.core.io.ClassPathResource;
 import org.springframework.core.io.Resource;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 import java.io.IOException;
+import java.nio.charset.StandardCharsets;
 
 @Controller
 public class HomeController {
-
     @GetMapping("/")
     @ResponseBody
     public String index() {
-				Resource resource = new ClassPathResource("/static/api-reference.json");
-				try {
-						ObjectMapper mapper = new ObjectMapper();
-						return mapper.readValue(resource.getInputStream(), Object.class);
-				} catch (IOException e) {
-						e.printStackTrace();
-				}
-				return null;
+			Resource resource = new ClassPathResource("/static/api-reference.json");
+			try {
+					return new String(resource.getInputStream().readAllBytes(), StandardCharsets.UTF_8);
+			} catch (IOException e) {
+					e.printStackTrace();
+			}
+			return "Could not find API reference";
     }
-
 }
