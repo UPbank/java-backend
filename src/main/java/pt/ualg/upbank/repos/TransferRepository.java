f98dff65be7e3ab684254cec8b51d775a1909f8e
sacha <106234329+oleksandrakiselova@users.noreply.github.com>
2022-09-26 14:57:53 +0100

    33-Filtrar Transações
    Falta ser revisto pelo João Sinfronio

--- src/main/java/pt/ualg/upbank/repos/TransferRepository.java
+++ src/main/java/pt/ualg/upbank/repos/TransferRepository.java
@@ -17,7 +17,33 @@ public interface TransferRepository extends JpaRepository<Transfer, Long> {
     Optional <List<Transfer>> 
     findByReceiverAndDateCreatedBetween(Account receiver, OffsetDateTime start, OffsetDateTime end);
 
-    
+    // Optional<List<Transfer>> findBySenderAndByDateCreatedGreaterThanEqualAndLessThanEqual(Account sender, OffsetDateTime start, OffsetDateTime end);
 
+    // Optional<List<Transfer>> findBySenderOrRecieverAndByDateCreatedGreaterThanEqualAndLessThanEqual(Account sender,Account reciever, OffsetDateTime start, OffsetDateTime end);
+
+    // public class TransferService {
+    //     public List<Transfer> findByConditions(
+    //         OffsetDateTime start,
+    //         OffsetDateTime end,
+    //         Long sender_id,
+    //         Long receiver_id,
+    //         String type,
+    //     ) {
+    //         if (start && end && start > end) {
+    //             OffsetDateTime temp_end = start;
+    //             start = end;
+    //             end = temp_end;
+    //         } 
+    
+    //         return tarnsfersList
+    //             .stream()
+    //             .filter(t -> start == null || t.getTransferDate() >= start)
+    //             .filter(t -> end == null || t.getTransferDate() <= end)
+    //             .filter(t -> sender_id == null || t.getSender().getId() == sender_id)
+    //             .filter(t -> receiver_id == null || t.getReceiver().getId() == receiver_id)
+    //             .filter(t -> type == null || t.getType() == type)
+    //             .toList()
+    //     }
+    // }
     boolean existsById(long id);
 }
